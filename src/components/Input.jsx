807d49888ce4baf86d899b17a72f4aa1188c0187
Thanda <tndbri001@myuct.ac.za>
2023-06-17 15:17:32 +0200

    Added the handleSubmit functionality.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,23 +1,51 @@
 import React, { useState } from 'react'
 
-const Input = () => {
-    const [item, setItem] = useState('');
+const Input = ({handleSubmit}) => {
+    const [text, setItem] = useState('');
+    const [day, setTime] = useState('');
+    const [reminder, setReminder] = useState(false);
 
     function handleChange(e) {
         setItem(e.target.value)
+    } 
+
+    function handleTime(e) {
+        setTime(e.target.value)
+    }
+
+    function onAdd(e) {
+        e.preventDefault();
+        
+        handleSubmit({text, day, reminder});
+
+        if(!text) {
+            alert('Please add task');
+            return
+        }
+
+        setItem('');
+        setTime('');
+        setReminder(false);
     }
 
   return (
-    <div style={{borderColor: 'black'}} className='mb-5 container'>
-        <div>
-        <label style={{fontWeight: '700'}} htmlFor="text" >Add Task</label>
-        <input style={{marginTop:'0'}} value={item} onChange={handleChange} className='form-control' type="text" />
+    <form className='add-form' onSubmit={onAdd} >
+        <div className='form-control'>
+        <label>Task</label>
+        <input placeholder='Add Task' value={text} onChange={handleChange} type="text" />
         </div>
-        <div>
-            <label style={{fontWeight: '700'}} htmlFor="text">Set Reminder</label>
-            <input type="text" name="reminder" className='form-control' />
+
+        <div className='form-control'>
+            <label>Day & Time</label>
+            <input placeholder='Add Day & Time' type="text" name="time" value={day} onChange={handleTime} />
         </div>
-    </div>
+
+        <div className='form-control form-control-check'>
+            <label>Set Reminder</label>
+            <input value={reminder} checked={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}  type="checkbox" />
+        </div>
+        <input className='btn btn-block' type='submit' value='Save Task' />
+    </form>
   )
 }
 
